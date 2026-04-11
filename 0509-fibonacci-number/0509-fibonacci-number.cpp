53879e6aceea871bb5eb05d8d07b592434dc47c5
Nova <beautymaurya9142@gmail.com>
2026-04-11 23:07:14 +0530

    Time: 0 ms (100%), Space: 7.9 MB (37.51%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,6 +4,14 @@ public:
         if(n<=1){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+       int a = 0, b = 1;
+
+    for(int i = 2; i <= n; i++) {
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return b;
     }
 };
