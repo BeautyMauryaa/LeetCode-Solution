61be902f01c75b2cbb2e60eb922394d61cf63b82
Nova <beautymaurya9142@gmail.com>
2026-04-17 23:53:12 +0530

    Time: 12 ms (6.27%), Space: 12.2 MB (7.97%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x<0){
+            return false;
+        }
+        vector<int> arr;
+        while(x>0){
+            int digit=x%10;
+            arr.push_back(digit);
+            x=x/10;
+        }
+
+        int i=0;
+        int j=arr.size()-1;
+        while(i<j){
+            if(arr[i]!=arr[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+        
+    }
+};
