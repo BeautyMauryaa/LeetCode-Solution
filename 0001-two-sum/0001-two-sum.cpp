d11a4eb57b77c9ac58ece8b4c313086d807d3fb1
Nova <beautymaurya9142@gmail.com>
2026-04-22 19:59:09 +0530

    Time: 81 ms (9.6%), Space: 14 MB (81.2%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
+        vector<int> index;
+        for(int i =0;i<nums.size();i++){
+            for(int j =i+1;j<nums.size();j++){
                 if(nums[i]+nums[j]==target){
-                    return{i,j};
+                    index.push_back(i);
+                    index.push_back(j);
+                    break;
                 }
             }
         }
 
-return{};
+        return index;
+
+        
     }
 };
