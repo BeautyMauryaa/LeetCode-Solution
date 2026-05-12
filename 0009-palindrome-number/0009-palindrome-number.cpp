5da9cd2d876e6528783a468dada5bb75e03f7f7a
Nova <beautymaurya9142@gmail.com>
2026-05-13 00:24:03 +0530

    Time: 0 ms (100%), Space: 8.6 MB (64.83%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0)) {
-            return false;
-        }
-
-        int reversedHalf = 0;
-
-        while (x > reversedHalf) {
-            reversedHalf = reversedHalf * 10 + x % 10;
-            x /= 10;
-        }
-        return (x == reversedHalf || x == reversedHalf / 10);
+       if(x<0){
+        return false;
+       }
+       int real=x;
+    long long rev=0;
+       while(x>0){
+        int digit=x%10;
+        rev=rev*10+digit;
+        x=x/10;
+       }
+       return real==rev;
     }
 };
