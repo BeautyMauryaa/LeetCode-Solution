6ae38b684592eacde8589c48237b81514bdd68ae
Nova <beautymaurya9142@gmail.com>
2026-06-03 23:32:02 +0530

    Time: 167 ms (70.32%), Space: 12.7 MB (35.62%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     int countPrimes(int n) {
+
+        if(n <= 2) return 0;
+
         vector<bool> prime(n, true);
-        int count = 0;
-        for (int i = 2; i < n; i++) {
-            if (prime[i]) {
-                count++;
-                for (long long j = 1LL * i * i; j < n; j += i) {
+
+        prime[0] = prime[1] = false;
+
+        for(int i = 2; i * i < n; i++) {
+
+            if(prime[i]) {
+
+                for(long long j = 1LL * i * i; j < n; j += i) {
                     prime[j] = false;
                 }
+
             }
         }
+
+        int count = 0;
+
+        for(int i = 2; i < n; i++) {
+            if(prime[i]) count++;
+        }
+
         return count;
     }
 };
