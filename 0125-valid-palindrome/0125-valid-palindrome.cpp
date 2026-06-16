e399271536d50bdd2a53ce739ddfaf888a6a8f16
Nova <beautymaurya9142@gmail.com>
2026-06-16 23:48:05 +0530

    Time: 3 ms (29%), Space: 10.2 MB (24.61%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     bool isPalindrome(string s){
-        string conversion;
-        for(char c : s){
-            if(isalnum(c)){
-                conversion+=tolower(c);
-            }
+       string conversion; //bcoz A!=a 
+       for(char c:s){
+        if(isalnum(c)){//number and alphabet
+            conversion+=tolower(c);
         }
-        int left=0;
-        int right=conversion.length()-1;
-        while(left<right){
-            if(conversion[left]!=conversion[right]){
-                return false;
-            }
-            left++;
-            right--;
+       }
+
+       int left=0;
+       int right=conversion.length()-1;
+       while(left<right){
+        if(conversion[left]!=conversion[right]){
+            return false;
         }
-        return true;
+        left++;
+        right--;
+       }
+       return true;
     }
 };
