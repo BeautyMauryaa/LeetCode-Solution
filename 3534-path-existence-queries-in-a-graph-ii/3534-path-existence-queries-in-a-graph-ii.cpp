62f412ec1341e7168f8fff4e96707b56eeb54677
Nova <beautymaurya9142@gmail.com>
2026-07-10 22:39:01 +0530

    Time: 165 ms (60.53%), Space: 367.2 MB (47.37%) - LeetHub

--- 3534-path-existence-queries-in-a-graph-ii/3534-path-existence-queries-in-a-graph-ii.cpp
+++ 3534-path-existence-queries-in-a-graph-ii/3534-path-existence-queries-in-a-graph-ii.cpp
@@ -1,49 +1,80 @@
 class Solution {
 public:
-    vector<int> pathExistenceQueries(int n, vector<int>& nums, int maxDiff,
-                                     vector<vector<int>>& queries) {
-        vector<int> idx(n), pos(n), res;
-        iota(idx.begin(), idx.end(), 0);
-        sort(idx.begin(), idx.end(),
-             [&](int a, int b) { return nums[a] < nums[b]; });
+    vector<int> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        vector<pair<int, int>> a;
+        for (int i = 0; i < n; i++)
+            a.push_back({nums[i], i});
+
+        sort(a.begin(), a.end());
+
+        // position of each original node in sorted order
+        vector<int> pos(n);
+        for (int i = 0; i < n; i++)
+            pos[a[i].second] = i;
+
+        // Connected components
+        vector<int> comp(n);
+        int id = 0;
+        comp[0] = 0;
+        for (int i = 1; i < n; i++) {
+            if (a[i].first - a[i - 1].first > maxDiff)
+                id++;
+            comp[i] = id;
+        }
+
+        // Farthest reachable in one step
+        vector<int> nxt(n);
+        int r = 0;
         for (int i = 0; i < n; i++) {
-            pos[idx[i]] = i;
+            while (r + 1 < n && a[r + 1].first - a[i].first <= maxDiff)
+                r++;
+            nxt[i] = r;
         }
 
-        int m = 32 - __builtin_clz(n);
-        vector<vector<int>> f(n, vector<int>(m));
+        const int LOG = 20;
+        vector<vector<int>> up(LOG, vector<int>(n));
 
-        for (int i = 0, left = 0; i < n; i++) {
-            while (nums[idx[i]] - nums[idx[left]] > maxDiff) left++;
-            f[i][0] = left;
-        }
+        for (int i = 0; i < n; i++)
+            up[0][i] = nxt[i];
 
-        for (int j = 1; j < m; j++) {
+        for (int k = 1; k < LOG; k++) {
             for (int i = 0; i < n; i++) {
-                f[i][j] = f[f[i][j - 1]][j - 1];
+                up[k][i] = up[k - 1][up[k - 1][i]];
             }
         }
 
-        for (auto& q : queries) {
-            auto [x, y] = pair(pos[q[0]], pos[q[1]]);
-            if (x > y) {
-                swap(x, y);
-            }
-            if (x == y) {
-                res.push_back(0);
+        vector<int> ans;
+
+        for (auto &q : queries) {
+            int u = pos[q[0]];
+            int v = pos[q[1]];
+
+            if (u == v) {
+                ans.push_back(0);
                 continue;
             }
 
-            int step = 0;
-            for (int i = m - 1; i >= 0; i--) {
-                if (f[y][i] > x) {
-                    y = f[y][i];
-                    step += 1 << i;
+            if (comp[u] != comp[v]) {
+                ans.push_back(-1);
+                continue;
+            }
+
+            if (u > v)
+                swap(u, v);
+
+            int cur = u;
+            int dist = 0;
+
+            for (int k = LOG - 1; k >= 0; k--) {
+                if (up[k][cur] < v) {
+                    cur = up[k][cur];
+                    dist += (1 << k);
                 }
             }
 
-            res.push_back(f[y][0] <= x ? step + 1 : -1);
+            ans.push_back(dist + 1);
         }
-        return res;
+
+        return ans;
     }
 };
